fed661282df0d1de0ae2940116bc9c3be0abbfdf
Remi <remi.peltriaux@edu.devinci.fr>
2024-03-22 10:56:39 +0100

    Client

--- Part1/app.py
+++ Part1/app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, jsonify
 from flask import Flask, request, render_template,jsonify
 from flask_cors import CORS
 from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError
-
+import json
+import csv
+import requests
 app = Flask(__name__)
 
 ENCRYPT_URL = 'http://localhost:5000/encrypt'
@@ -11,7 +12,7 @@ DECRYPT_URL = 'http://localhost:5000/decrypt'
 dbFile = 'credentials.json'
 database = []
 
-hasher = argon2.PasswordHasher(hash_len=256)
+hasher = PasswordHasher(hash_len=256)
 
 def load_json_file(jsonfile):
     with open(jsonfile, 'r') as file:
@@ -20,7 +21,14 @@ def load_json_file(jsonfile):
         user = {'id': entry['id'], 'pwd': entry['pwd']}
         database.append(user)
     return database
-def (update_jsonfile):
+def update_json_file(bodyJson):
+    id = json.body_json.get('id')
+    pwd = json.body_json.get('pwd')
+
+    with open(dbFile, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([id, pwd])
+
 
 
 @app.route('/register', methods=['POST'])
@@ -29,7 +37,7 @@ def register():
     data = request.json
     id = request.json['id']
     pwd= request.json['pwd']
-    load_json_file(dbfile)
+    load_json_file(dbFile)
 
     if id in [x[0] for x in database]:
         return jsonify({'message': 'User already exists'}), 200
@@ -47,7 +55,7 @@ def login():
     data = request.json
     id= request.json['id']
     pwd= request.json['pwd']
-    load_json_file(dbfile)
+    load_json_file(dbFile)
 
     # Check is the user is registered
     if id not in [x[0] for x in database]:
@@ -56,16 +64,15 @@ def login():
     encrypted_hash = (x[1] for x in database if x[0] == id)
     if encrypted_hash is None:
         return jsonify({'message' : 'Error during password recovery' }), 200
-
+    
     body = {
         'id': id,
         'encrypted_hash': encrypted_hash
         }
-
     response = requests.post(DECRYPT_URL, json=body)
-    hashToVerify = response.json().get('hash')
-    # Verify password
-    if password == decrypted_password:
+    decrypted_hash = response.json().get('hash')
+    # Verify password (pwd is the password in plain that the user entered, decrypted_hash is the hash of the real password stored in the database)
+    if hasher.verify(pwd, decrypted_hash):
         return jsonify({'message': 'Login successful'}), 200
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
