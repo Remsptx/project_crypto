7973b0b918382cded237d929e3c35d88cbb0c9f8
Remi <remi.peltriaux@edu.devinci.fr>
2024-03-29 21:59:18 +0100

    Connexion refused

--- Part1/client/app.py
+++ Part1/client/app.py
@@ -9,8 +9,8 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-ENCRYPT_URL = 'http://localhost:3001/encrypt'
-DECRYPT_URL = 'http://localhost:3001/decrypt'
+ENCRYPT_URL = 'http://Server:5000/encrypt'
+DECRYPT_URL = 'http://Server:5000/decrypt'
 dbFile = 'credentials.json'
 database = []
 
